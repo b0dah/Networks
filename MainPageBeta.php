9d8e2adadd0e349634301a066de2c5da9005dad7
Evan <ries8898@gmial.com>
2019-04-03 23:03:18 +0300

    added site file

---
+++ MainPageBeta.php
@@ -0,0 +1,13 @@
+
+<html>
+<head>
+	<title></title>
+</head>
+
+
+<body>
+	<h3>privet</h3>
+
+
+</body>
+</html>
---
+++ php_hello_world.php
@@ -0,0 +1,14 @@
+
+<html>
+<head>
+	<title>
+		<p> here is some php </p>
+	</title>
+</head>
+
+
+<body>
+	<?php echo "<p> Hello, World! <p>"; ?>
+
+</body>
+</html>
