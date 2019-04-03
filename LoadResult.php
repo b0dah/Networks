2122cece8d4d62a9c1f31d00737cb2ca16f233c3
Evan <ries8898@gmial.com>
2019-04-04 00:00:52 +0300

    New struct

---
+++ LoadResult.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<p> <?php  echo $_GET['destination'] ?> </p>
+
+</body>
+</html>
---
+++ MainPage.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+	<title></title>
+</head>
+
+<body>
+	<br> <br>
+
+	<form action="LoadResult.php">
+		<select name=destination size = 1 >
+			<option value=1> Paris </option>
+			<option value=2 selected> Vien </option>
+			<option value=3> Milan </option>
+			
+		</select>
+		<input type="submit">
+	</form>
+
+</body>
+</html>
