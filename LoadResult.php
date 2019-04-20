af888d79cbbe82420e6df2b4b073cbc2a5afb3fc
Evan <ries8898@gmial.com>
2019-04-20 22:59:42 +0300

    bad design for start page done

--- LoadResult.php
+++ LoadResult.php
@@ -1,11 +1,19 @@
-<html>
+<!--<html>
 <head>
 	<title></title>
 </head>
 <body>
-	<p> <?php  echo $_GET['origin'] ?> </p>
 
-	<p> <?php  echo $_GET['destination'] ?> </p>
-	
 </body>
 </html>
+
+	<p> <?php   echo $_GET['origin'] ?> </p>
+
+	<p> <?php  echo $_GET['destination'] ?> </p>
+-->
+
+<?php 
+	echo $_GET['origin'] . " -> " . $_GET['destination'];
+
+
+?>
--- MainPage.php
+++ MainPage.php
@@ -1,20 +1,14 @@
 <html>
 <head>
-	<title></title>
+	<title>** Main page **</title>
+		<link href="styles/style.css" rel="stylesheet" type="text/css"> 
 </head>
 
 <body>
-	<br> <br>
+	
+</body>
+</html>
 
-	<form action="LoadResult.php">
-		<select name=destination size = 1 >
-			<option value="Paris"> Paris </option>
-			<option value="Vein" selected> Vein </option>
-			<option value="Milan"> Milan </option>
-			
-		</select>
-		<input type="submit" value="get it">
-	</form>
 
 <?php
 	include 'db_library.php';
@@ -22,21 +16,23 @@
 	$connection = new DataBase();
 	$sql_line = "SELECT * FROM Flights";
 
-	//$connection->print_select();
 
+	
 	echo "<form action= \"LoadResult.php\" >";
-
-		echo " <br>	     From <br>";
+		
+		echo " 	 <h3>    From  </h3> ";
 		$result = $connection->query($sql_line);
 		$connection->create_select_form("origin", $result, 'no', 'from');
 
 
-		echo " <br>	     To <br>";
+		echo "  <h3>    To  </h3> ";
 		$connection->create_select_form("destination", $result, 'no', 'to');
+		
+		echo "<br><br>";
 
-		echo "<br><br> <input type= \"submit\" value= \"get it\"> ";
+		echo  "<input id=\"submit_button\" type= \"submit\" value= \"get it\" >"; 
+
 	echo "</form>";
+	
 ?>
 
-</body>
-</html>
